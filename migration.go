bc66c42d2263edc357fb636251c01d8ba4544e93
Jack Shaw <jack.shaw@canonical.com>
2022-04-08 15:49:11 +0100

    Quality of Life improvements
    Read ddl statements from a separate file

--- migration.go
+++ migration.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"io/ioutil"
 	"log"
+	"strings"
 )
 
 type migration struct {
@@ -15,8 +17,13 @@ func (m *migration) migrate() error {
 		return err
 	}
 
-	for _, stmt := range m.statements() {
-		log.Printf("Running statement:%s\n", stmt)
+	stmts, err := m.statements()
+	if err != nil {
+		return err
+	}
+
+	for _, stmt := range stmts {
+		log.Printf("Running statement:\n%s\n\n", stmt)
 
 		if _, err := txn.Exec(stmt); err != nil {
 			_ = txn.Rollback()
@@ -32,39 +39,11 @@ func (m *migration) migrate() error {
 	return nil
 }
 
-func (m *migration) statements() []string {
-	return []string{`
-CREATE TABLE IF NOT EXISTS machine (
-	uuid 			TEXT PRIMARY KEY,
-	id 				TEXT NOT NULL UNIQUE,
-	nonce 			TEXT,
-	series 			TEXT,
-	container_type	TEXT
-);
-`, `
-CREATE UNIQUE INDEX idx_machine_id
-ON machine (id);
-`, `
-CREATE TABLE IF NOT EXISTS filesystem (
-	uuid 			TEXT PRIMARY KEY,
-	id 				TEXT NOT NULL UNIQUE,
-	releasing		BOOLEAN
-);
-`, `
-CREATE TABLE IF NOT EXISTS machine_filesystem (
-	uuid 			TEXT PRIMARY KEY,
-	machine_id		TEXT NOT NULL,
-	filesystem_id	TEXT NOT NULL,
-	CONSTRAINT 		fk_machine_filesystem_machine
-		FOREIGN KEY (machine_id)
-		REFERENCES 	machine(id),
-	CONSTRAINT 		fk_machine_filesystem_filesystem
-		FOREIGN KEY (filesystem_id)
-		REFERENCES 	filesystem(id)
-);
-`, `
-CREATE UNIQUE INDEX idx_machine_filesystem_mid_fid
-ON machine_filesystem (machine_id, filesystem_id);
-`,
+func (m *migration) statements() ([]string, error) {
+	bytes, err := ioutil.ReadFile("schema.ddl")
+	if err != nil {
+		return nil, err
 	}
+	stmts := strings.Split(string(bytes), "\n\n")
+	return stmts, nil
 }
